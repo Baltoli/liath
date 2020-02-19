361a5aed2cc6946c6d4287d8a3497e132c260bf4
Bruce Collie <brucecollie82@gmail.com>
2020-02-19 11:40:06 +0000

    Configurable gemv

--- test.cpp
+++ test.cpp
@@ -1,5 +1,8 @@
+#include <algorithm>
 #include <array>
 #include <iostream>
+#include <random>
+#include <vector>
 
 template <typename T>
 class proxy {
@@ -55,16 +58,35 @@ void gemv(size_t M, size_t N, ptr<T> A, ptr<T> x, ptr<T> y) {
   }
 }
 
-int main() {
-  auto A = std::array{1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
-  auto x = std::array{7.0, 8.0};
-  auto y = std::array{0.0, 0.0, 0.0};
+int main(int argc, char **argv) {
+  if (argc < 3) {
+    std::cout << "Usage: " << argv[0] << " M N\n";
+    return 1;
+  }
+
+  auto m = std::stoul(argv[1]);
+  auto n = std::stoul(argv[2]);
+
+  auto A = std::vector<double>(m * n);
+  auto x = std::vector<double>(n);
+  auto y = std::vector<double>(m);
+
+  auto rd = std::random_device{};
+  auto rand_val = [&rd]() {
+    auto gen = std::mt19937(rd());
+    auto dist = std::uniform_real_distribution<>(0.0, 1.0);
+    return dist(gen);
+  };
+
+  std::generate(A.begin(), A.end(), rand_val);
+  std::generate(x.begin(), x.end(), rand_val);
+  std::generate(y.begin(), y.end(), rand_val);
 
   auto Ap = ptr(A.data());
   auto xp = ptr(x.data());
   auto yp = ptr(y.data());
 
-  gemv(3, 2, Ap, xp, yp);
+  gemv(m, n, Ap, xp, yp);
 
   std::cout << "[";
   auto comma = "";
